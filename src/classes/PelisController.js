deff2b961b499a8b730c423701b10ac46723a35b
adrianvicoh <1hiaw.vico.adrian@gmail.com>
2024-06-05 00:24:42 +0200

    Añadidos controllers, layouts, components, datos, iniciada

---
+++ src/classes/PelisController.js
@@ -0,0 +1,98 @@
+const APIURL = 'https://app.nocodb.com/api/v2/tables/mawokzemkiqssco/records';
+const TOKEN = '4rfGfqNky6ukuBn9NgtrnkC8DPERQjvtknpzizyS';
+
+class PelisController {
+    constructor() {
+        this.apiUrl = APIURL;
+        this.token = TOKEN;
+    }
+
+    async getAll() {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            }
+        });
+
+        return response.json();
+    }
+
+    async createItem(title, year, rate, genre, director, cast, description) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                identificador: parseInt(Math.random() * 10),
+                título: title,
+                Año: year,
+                clasificación: rate,
+                género: genre,
+                director: director,
+                protagonistas: cast,
+                descripción: description
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async updateItem(title, year, rate, genre, director, cast, description) {
+        const response = await fetch(`${this.apiUrl}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                identificador: id,
+                título: title,
+                Año: year,
+                clasificación: rate,
+                género: genre,
+                director: director,
+                protagonistas: cast,
+                descripción: description
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async getItemById(id) {
+        const response = await fetch(`${this.apiUrl}/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            }
+        });
+
+        const data = await response.json();
+        return data;
+    }
+
+    async deleteItem(id) {
+        const response = await fetch(`${this.apiUrl}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'xc-token': this.token
+            },
+            body: JSON.stringify({
+                Id: id
+            })
+        });
+
+        const data = await response.json();
+        return data;
+    }
+}
+
+export default PelisController;
